dd54be2cfb555091ccef7e8aa8eb0e4bc39e4cb0
pszyszkowska <szyszkowskapatrycja@gmail.com>
2024-06-17 00:18:34 +0200

    final version

--- FlightReservation/settings.py
+++ FlightReservation/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sjs348*)@dk@((#n^rjp-m*^$4fu0xj@=x-)x(vqcd2azg4n_%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 

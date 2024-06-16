d4b8c919af572cfdd8fa4f173a00fec503e8c8b4
pszyszkowska <szyszkowskapatrycja@gmail.com>
2024-06-17 00:02:23 +0200

    final version

--- client/views.py
+++ client/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+import re
+
 from django.contrib import messages
 from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -10,10 +12,21 @@ def clientRegistration(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            result = form.save()
+            print("test")
+            print(result)
             messages.success(request, 'Successfully registered, now u can log in')
             return redirect("/flightSearch")
+        else:
+            reformattedErrors = str(form.errors).replace("<ul class=\"errorlist\">", "")
+            reformattedErrors = re.sub("<li>.*?<li>", "", reformattedErrors)
+            reformattedErrors = reformattedErrors.replace("</li></ul></li></ul>", "")
+            reformattedErrors = reformattedErrors.split("</li></ul></li>")
+            for error in reformattedErrors:
+                messages.add_message(request, messages.ERROR, error)
+            return render(request, "registration.html")
     else:
+
         form = UserCreationForm()
 
     return render(request, "registration.html", {"form": form})
